e92edae0b0eaa062aa244645e374450cd84be25a
iantzoulis <ikantzoulis@gmail.com>
2021-10-16 18:16:53 -0400

    Added artist and track dataframes

--- scripts/main.py
+++ scripts/main.py
@@ -18,8 +18,37 @@ if __name__ == "__main__":
         album_elem = {'album_id':album_id, 'name':album_name, 'release_date':album_release_date, 'url':album_url, 'artist_id':artist_id}
         albums_list.append(album_elem)
 
-    #Convert albums data to pandas DataFrame and drop duplicates
+    artists_arr = []
+    for row in recently_played['items']:
+        artist_id = row['track']['artists'][0]['id']
+        artist_name = row['track']['artists'][0]['name']
+        artist_url = row['track']['artists'][0]['external_urls']['spotify']
+
+        artist_elem = {'artist_id':artist_id, 'name':artist_name, 'url':artist_url}
+        artists_arr.append(artist_elem)
+
+    tracks_arr = []
+    for row in recently_played['items']:
+        track_id = row['track']['id']
+        track_name = row['track']['name']
+        track_duration = row['track']['duration_ms']
+        track_url = row['track']['external_urls']['spotify']
+        album_id = row['track']['album']['id']
+        artist_id = row['track']['album']['artists'][0]['id']
+        
+        track_elem = {'track_id':track_id, 'name':track_name, 'duration':track_duration, 'url':track_url, 'album_id':album_id, 'artist_id':artist_id}
+        tracks_arr.append(track_elem)
+
+    #Convert list objects to pandas DataFrame and drop duplicates
     albums_df = pd.DataFrame.from_dict(albums_list)
     albums_df = albums_df.drop_duplicates(subset=['album_id'])
 
-    print(albums_df)
+    artists_df = pd.DataFrame.from_dict(artists_arr)
+    artists_df = artists_df.drop_duplicates(subset=['artist_id'])
+
+    tracks_df = pd.DataFrame.from_dict(tracks_arr)
+    tracks_df = tracks_df.drop_duplicates(subset=['track_id'])
+
+    #print(albums_df)
+    #print(artists_df)
+    #print(tracks_df)
